ed979c1e15c183e58f457aa84864baec8d6bd4d5
Shlumpus <140124184+Shlumpus@users.noreply.github.com>
2024-06-14 00:39:37 +0000

    account actions and setting up a bank manager

--- account.py
+++ account.py
@@ -4,4 +4,36 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
+class Account:
+    def __init__(self, name, balance, password, number):
+        self._name = name
+        self._balance = balance
+        self._password = password
+        self._number = number
+        
+    def get_account_number(self, number):
+        return self._number
+        
+    def get_account_name(self, name):
+        return self._name  
+    
+    def get_account_password(self, password):
+        return self._password
+        
+    def get_balance(self, balance):
+        return self._balance    
+        
+    def change_password(self, password, new_password):
+        password = new_password
+        
+    def deposit(self, addition):
+        self._get_balance()
+        self._balance += addition
+        
+    def withdraw(self, balance, subtraction):
+        if subtraction < balance:
+            self._get_balance()
+            self._balance -= subtraction
+        else:
+            print("You have insufficient funds, withdrawal failed")
+        
--- bank.py
+++ bank.py
@@ -4,3 +4,21 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+from account import Account
+
+class BankManager():
+    def __init__(self):
+        self.accounts = []
+        
+    def add_account(self, name, password):
+        balance = '0'
+        new_account = Account(name, balance, password)
+        self.accounts.append(new_account)
+        
+    def remove_account(self):
+        get_account_number(self)
+        
+        
+    def display_accounts(self):
+        print()
